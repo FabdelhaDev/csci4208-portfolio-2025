c5de91eea8eb5ca7eb1e54f23f05312f833ef432
FabdelhaDev <fabdelhadev@gmail.com>
2025-10-06 19:40:06 -0500

    Adding Lab-06

---
+++ labs/lab-06/README.txt
@@ -0,0 +1,3 @@
+Hello! This is a QUIZ, with the original code authored by Ted Holmberg, and now written by Fares Abdelhamed!
+
+Simply run the Index.HTML file, run an http-server, and then open localhost:8080 and play through the quiz to get a high score!
---
+++ labs/lab-06/scripts/components/Leaderboard.js
@@ -0,0 +1,19 @@
+const Leaderboard = (topScores) => (
+    `<h2>Top Scores:</h2>
+    <section>
+        <ol>
+            ${ ListItems(topScores) }
+        <ol>
+    </section>`
+);
+
+const ListItems = (topScores) => {
+    let li = ``;
+    const scores = topScores.sort( (a,b) => b.score - a.score);
+    for(let row of scores){
+        li += `<li>${row.name}: ${row.score}</li>`
+    }
+    return li;
+}
+
+export default Leaderboard;
